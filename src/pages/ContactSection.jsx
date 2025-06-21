c0a9c589ea485b8a60790e79c7ca424008e4cf51
shumei <kekezuiaidesky@gmail.com>
2025-06-21 19:04:22 +0200

    contact copy part

--- src/pages/ContactSection.jsx
+++ src/pages/ContactSection.jsx
@@ -12,7 +12,7 @@ export default function ContactSection() {
   };
 
   return (
-    <section className="h-[25vh] sm:h-screen w-[100vw] text-black">
+    <section className="h-[25vh] sm:h-screen w-[100vw] text-black relative">
       {/* 顶部箭头 */}
       <img
         src="/src/assets/ContactSection/Arrowline1.svg"
@@ -21,7 +21,7 @@ export default function ContactSection() {
       />
 
       {/* 中间主内容 */}
-      <div className="bg-[#05A552] h-[20vh] sm:h-[90vh] px-10 sm:px-20 flex flex-col gap-8">
+      <div className="bg-[#05A552] h-[20vh] sm:h-[90vh] px-10 sm:px-20 flex flex-col gap-8 relative">
         {/* Row 1: 标题 + 两个按钮 */}
         <div className="flex flex-wrap justify-between items-center gap-4">
           <h2 className="text-[3vw] sm:text-[7vw] font-extrabold">
@@ -32,8 +32,7 @@ export default function ContactSection() {
               href="https://www.linkedin.com/in/shumei-zhang-5a8612231/"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-[#FFC713] text-black
-              text-[2vw] sm:text-[3vw] font-semibold px-4 py-2 
+              className="bg-[#FFC713] text-black text-[2vw] sm:text-[3vw] font-semibold px-4 py-2 
               border-2 border-black rounded-lg shadow"
             >
               Linkedin
@@ -42,8 +41,7 @@ export default function ContactSection() {
               href="https://github.com/shumeiya"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-[#7A78FF]  text-black 
-              text-[2vw] sm:text-[3vw] font-semibold px-4 py-2 
+              className="bg-[#7A78FF] text-black text-[2vw] sm:text-[3vw] font-semibold px-4 py-2 
               border-2 border-black rounded-lg shadow"
             >
               Github
@@ -51,22 +49,34 @@ export default function ContactSection() {
           </div>
         </div>
 
-        {/* Row 2: 邮箱行 */}
-        <div
-          className="bg-white text-black px-6 py-[1vw] rounded-xl border-2  border-black
-          flex items-center justify-between 
-        text-[2vw] sm:text-[3vw] font-bold "
-        >
-          <span>{email}</span>
-          <button
-            onClick={handleCopy}
-            className="bg-[#05A552] border-2 border-black px-[2vw] py-[0.5vw]"
+        {/* Row 2: 邮箱行*/}
+        <div className="relative w-full">
+          <div
+            className="bg-white text-black px-6 py-[1vw] rounded-xl border-2 border-black
+            flex items-center justify-between w-full
+            text-[2vw] sm:text-[3vw] font-bold"
           >
-            <img src="/src/assets/ContactSection/Copy.svg"></img>
-          </button>
+            <span>{email}</span>
+            <button
+              onClick={handleCopy}
+              className="bg-[#05A552] border-2 border-black px-[2vw] py-[0.5vw]"
+            >
+              <img src="/src/assets/ContactSection/Copy.svg" alt="Copy" />
+            </button>
+          </div>
+
+          {/* 提示复制成功 */}
+          {copied && (
+            <div
+              className="absolute top-full mt-2 left-1/2 -translate-x-1/2
+              bg-black text-white text-sm sm:text-lg px-4 py-2 rounded-xl z-50 shadow-lg"
+            >
+              Copied to clipboard!
+            </div>
+          )}
         </div>
 
-        {/* Row 3: Flex布局 - 左 */}
+        {/* Row 3: 左侧文字 + 右侧人物 */}
         <div className="flex flex-col sm:flex-row text-white items-center justify-between mt-6 gap-6">
           <FallingText
             text={`Interactive Critical Collaboration Teamwork Empathy`}
@@ -79,8 +89,6 @@ export default function ContactSection() {
             fontSize="3rem"
             mouseConstraintStiffness={0.9}
           />
-
-          {/* 右侧头像 */}
           <div className="w-[17vw]">
             <img src="/src/assets/ContactSection/Work.png" alt="Mascot" />
           </div>
@@ -93,13 +101,6 @@ export default function ContactSection() {
         alt="Bottom Arrows"
         className="pl-[20vw]"
       />
-
-      {/* 提示复制成功 */}
-      {copied && (
-        <div className="absolute bottom-4 left-1/2 -translate-x-1/2 bg-black text-white text-sm px-4 py-2 rounded-xl">
-          Copied to clipboard!
-        </div>
-      )}
     </section>
   );
 }
