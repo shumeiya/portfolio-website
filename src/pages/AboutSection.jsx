c4572fe29b9d57aaadad4468192658337ed2b377
shumei <kekezuiaidesky@gmail.com>
2025-06-30 00:55:01 +0200

    滚动字体动画

--- src/pages/AboutSection.jsx
+++ src/pages/AboutSection.jsx
@@ -1,3 +1,5 @@
+import ScrollFloatBlock from "../blocks/TextAnimations/ScrollFloat/ScrollFloat";
+
 export default function StatementSection() {
   return (
     <section
@@ -5,57 +7,59 @@ export default function StatementSection() {
     flex justify-center items-center
     px-[2vw] py-[1vw] "
     >
-      <div className="px-[2vw] ">
-        {/* Row 1 */}
-        <div className="flex justify-center items-center gap-x-4 flex-wrap text-center">
-          <p className="text-[4vw] font-extrabold">I dream, I design,</p>
-          <img
-            src="/src/assets/AboutSection/AnimalCrossing.png"
-            alt="Animal Crossing"
-            className="w-[9vw]"
-          />
-          <p className="text-[4vw] font-extrabold">I create!</p>
-        </div>
+      <ScrollFloatBlock>
+        <div className="px-[2vw] ">
+          {/* Row 1 */}
+          <div className="flex justify-center items-center gap-x-4 flex-wrap text-center">
+            <p className="text-[4vw] font-extrabold">I dream, I design,</p>
+            <img
+              src="/src/assets/AboutSection/AnimalCrossing.png"
+              alt="Animal Crossing"
+              className="w-[9vw]"
+            />
+            <p className="text-[4vw] font-extrabold">I create!</p>
+          </div>
 
-        {/* Row 2 */}
-        <div className="flex justify-center items-center gap-x-4 flex-wrap text-center">
-          <p className="text-[4vw] font-extrabold">With play</p>
-          <img
-            src="/src/assets/AboutSection/Switch.gif"
-            alt="switch"
-            className="w-[9vw]"
-          />
-          <p className="text-[4vw] font-extrabold">and flair,</p>
-          <img
-            src="/src/assets/AboutSection/Me.gif"
-            alt="Girl"
-            className="w-[9vw]"
-          />
-        </div>
+          {/* Row 2 */}
+          <div className="flex justify-center items-center gap-x-4 flex-wrap text-center">
+            <p className="text-[4vw] font-extrabold">With play</p>
+            <img
+              src="/src/assets/AboutSection/Switch.gif"
+              alt="switch"
+              className="w-[9vw]"
+            />
+            <p className="text-[4vw] font-extrabold">and flair,</p>
+            <img
+              src="/src/assets/AboutSection/Me.gif"
+              alt="Girl"
+              className="w-[9vw]"
+            />
+          </div>
 
-        {/* Row 3 */}
-        <div className="flex justify-center items-center gap-x-4 flex-wrap text-center">
-          <p className="text-[4vw] font-extrabold">I shape ideas into</p>
-          <img
-            src="/src/assets/AboutSection/Swimming.gif"
-            alt="Swimming"
-            className="w-[9vw]"
-          />
-          <p className="text-[4vw] font-extrabold">design.</p>
-        </div>
+          {/* Row 3 */}
+          <div className="flex justify-center items-center gap-x-4 flex-wrap text-center">
+            <p className="text-[4vw] font-extrabold">I shape ideas into</p>
+            <img
+              src="/src/assets/AboutSection/Swimming.gif"
+              alt="Swimming"
+              className="w-[9vw]"
+            />
+            <p className="text-[4vw] font-extrabold">design.</p>
+          </div>
 
-        {/* Row 4 */}
-        <div className="flex justify-between items-end  px-4">
-          <img
-            src="/src/assets/AboutSection/AnimalCrossing.gif"
-            alt="Animal Crossing"
-            className="w-[9vw]"
-          />
-          <p className="text-right font-light text-[3vw] w-full text-white">
-            I am Shumi~Ya !
-          </p>
+          {/* Row 4 */}
+          <div className="flex justify-between items-end  px-4">
+            <img
+              src="/src/assets/AboutSection/AnimalCrossing.gif"
+              alt="Animal Crossing"
+              className="w-[9vw]"
+            />
+            <p className="text-right font-light text-[3vw] w-full text-white">
+              I am Shumi~Ya !
+            </p>
+          </div>
         </div>
-      </div>
+      </ScrollFloatBlock>
     </section>
   );
 }
