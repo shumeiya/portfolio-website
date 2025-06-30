849997197b8ecb6e5bfe39c6aaf310b8e6887ad1
shumei <kekezuiaidesky@gmail.com>
2025-06-30 18:08:32 +0200

    ProjectSection2 mockup

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useRef, useEffect } from "react";
+import gsap from "gsap";
+
+const ProjectItem = ({
+  name,
+  icons,
+  bgImage,
+  bgImageHover,
+  MockupImage,
+  MockupImageClassName = "",
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const iconRefs = useRef([]);
+
+  useEffect(() => {
+    if (isHovered) {
+      const positions = [
+        { x: -10, y: -130 },
+        { x: 800, y: -100 },
+        { x: -400, y: 100 },
+        { x: 600, y: 100 },
+      ];
+      iconRefs.current.forEach((ref, index) => {
+        const { x, y } = positions[index % positions.length];
+        gsap.to(ref, {
+          x,
+          y,
+          duration: 0.5,
+          ease: "back.out(1)",
+        });
+        gsap.to(ref, {
+          y: y - 15,
+          duration: 1,
+          yoyo: true,
+          repeat: -1,
+          ease: "sine.inOut",
+        });
+      });
+    } else {
+      iconRefs.current.forEach((ref) => {
+        gsap.killTweensOf(ref);
+        gsap.to(ref, {
+          x: 0,
+          y: 0,
+          duration: 0.5,
+          ease: "back.in(1.7)",
+        });
+      });
+    }
+  }, [isHovered]);
+
+  const MockupImageRef = useRef();
+  useEffect(() => {
+    if (!MockupImageRef.current) return;
+    gsap.killTweensOf(MockupImageRef.current);
+    gsap.to(MockupImageRef.current, {
+      opacity: isHovered ? 1 : 0,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  }, [isHovered]);
+
+  const backgroundImage = isHovered ? bgImageHover : bgImage;
+
+  return (
+    <div
+      className={`relative w-[90vw] h-[9vw] left-1/2 -translate-x-1/2
+      transition-all duration-300 flex  justify-start items-center overflow-visible ${
+        isHovered ? "z-50" : "z-1"
+      }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "auto 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* Floating Icons */}
+      {icons.map((icon, i) => (
+        <img
+          key={i}
+          src={icon}
+          ref={(el) => (iconRefs.current[i] = el)}
+          className={`absolute left-[30vw] w-[7vw] h-[7vw] pointer-events-none transition-opacity ${
+            isHovered ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      ))}
+      {/* Mockup Image */}
+      <img
+        src={MockupImage}
+        ref={MockupImageRef}
+        className={`absolute left-1/2 -translate-x-1/2
+          pointer-events-none ${MockupImageClassName}`}
+        style={{ opacity: 0, transition: "opacity 0.05s" }}
+        alt="extra"
+      />
+      {/* Project Title */}
+      <div
+        className={`text-[4vw] font-regular pl-[5vw] transition-colors duration-300  ${
+          isHovered ? "text-white" : "text-black"
+        }`}
+      >
+        {name}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- src/pages/ProjectsSection2.jsx
+++ src/pages/ProjectsSection2.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import SlowFoodItem from "../components/ProjectSection2Item/SlowFood.jsx";
+import ProjectItem from "../components/ProjectItem.jsx";
 
 export default function ProjectsSection() {
   return (
-    <section className="h-[35vh] sm:h-[120vh] w-[100vw] relative pt-[30vw] sm:py-[15vw]">
+    <section className="h-[20vh] sm:h-[90vh] w-[100vw] relative  sm:pt-[5vh]">
       <div className="flex flex-col justify-center">
-        <SlowFoodItem
+        <ProjectItem
           name="SlowFood"
           icons={[
             "/src/assets/ProjectSection2/SlowFood/Eggplant.svg",
@@ -13,8 +13,12 @@ export default function ProjectsSection() {
             "/src/assets/ProjectSection2/SlowFood/Pepper.svg",
             "/src/assets/ProjectSection2/SlowFood/Tomato.svg",
           ]}
+          bgImage="/src/assets/ProjectSection2/WhiteArrow.svg"
+          bgImageHover="/src/assets/ProjectSection2/SlowFood/GreenArrow.svg"
+          MockupImage="/src/assets/ProjectSection2/SlowFood/Mockup.png"
+          MockupImageClassName="h-[10vh] sm:h-[40vh] w-auto bottom-0"
         />
-        <SlowFoodItem
+        <ProjectItem
           name="Jigsaw Puzzle App"
           icons={[
             "/src/assets/ProjectSection2/JigsawPuzzle/1.svg",
@@ -22,8 +26,12 @@ export default function ProjectsSection() {
             "/src/assets/ProjectSection2/JigsawPuzzle/3.svg",
             "/src/assets/ProjectSection2/JigsawPuzzle/4.svg",
           ]}
+          bgImage="/src/assets/ProjectSection2/WhiteArrow.svg"
+          bgImageHover="/src/assets/ProjectSection2/JigsawPuzzle/PurpleArrow.svg"
+          MockupImage="/src/assets/ProjectSection2/JigsawPuzzle/PuzzleMockup.png"
+          MockupImageClassName="h-[10vh] sm:h-[40vh] w-auto ml-[10vw] bottom-0"
         />
-        <SlowFoodItem
+        <ProjectItem
           name="TIDE Website"
           icons={[
             "/src/assets/ProjectSection2/TIDE/1.svg",
@@ -31,15 +39,23 @@ export default function ProjectsSection() {
             "/src/assets/ProjectSection2/TIDE/3.svg",
             "/src/assets/ProjectSection2/TIDE/4.svg",
           ]}
+          bgImage="/src/assets/ProjectSection2/WhiteArrow.svg"
+          bgImageHover="/src/assets/ProjectSection2/TIDE/BluePurpleArrow.svg"
+          MockupImage="/src/assets/ProjectSection2/TIDE/TIDEMockup.png"
+          MockupImageClassName="h-[10vh] sm:h-[30vh] w-auto ml-[10vw] bottom-0"
         />
-        <SlowFoodItem
-          name="JiDu HMI Design"
+        <ProjectItem
+          name="HMI Design"
           icons={[
             "/src/assets/ProjectSection2/HMI/1.svg",
             "/src/assets/ProjectSection2/HMI/2.svg",
             "/src/assets/ProjectSection2/HMI/3.svg",
             "/src/assets/ProjectSection2/HMI/4.svg",
           ]}
+          bgImage="/src/assets/ProjectSection2/WhiteArrow.svg"
+          bgImageHover="/src/assets/ProjectSection2/HMI/LightBlueArrow.svg"
+          MockupImage="/src/assets/ProjectSection2/HMI/HMIMockup.png"
+          MockupImageClassName="h-[10vh] sm:h-[25vh] w-auto ml-[5vw] bottom-0"
         />
       </div>
     </section>
